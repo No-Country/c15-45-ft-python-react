2789b414b6f2a22cd8e817c8347d7f7046b943ee
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-12-14 21:22:39 -0300

    [Update] Responsividad en listado de tiendas

--- NoStoreBehind/frontend/src/components/store-card.tsx
+++ NoStoreBehind/frontend/src/components/store-card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 interface StoreCardProps {
   name: string;
@@ -11,25 +12,28 @@ function StoreCard({ name, description, productsLength }: StoreCardProps) {
   console.log(productsLength);
   return (
     <div className="flex w-full flex-col gap-2.5 rounded-lg bg-white p-3">
+      <div className="relative h-36 w-full">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png"
+          fill
+          className="h-full w-full rounded-md object-cover object-center"
+          alt="Card Image"
+        />
+      </div>
       <div className="flex w-full items-center justify-between">
         <div className="flex h-full max-h-20 flex-col items-start self-start overflow-hidden">
           <h3 className="text-lg font-semibold">{name}</h3>
           <p className="text-sm">{description}</p>
         </div>
-
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png"
-          width={100}
-          height={100}
-          className="rounded-md"
-          alt="Card Image"
-        />
       </div>
       <Separator />
       <div className="flex items-center">
         <p className="text-[13px] text-primary">
           Cant. Prod: <span>{productsLength}</span>
         </p>
+        <Button variant="outline" size="sm" className="ml-auto">
+          Ver Tienda
+        </Button>
       </div>
     </div>
   );
